4c44c77e781fc877423a0b3fdaa63ab6bc14e111
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-01-16 23:09:49 +0600

    Project updated

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ function App() {
     <>
       <div className="max-w-7xl mx-auto">
         <h1 className="text-5xl font-bold py-5 my-5 bg-blue-200 rounded-lg">
-          Courses of Freelancer Nasim
+          Courses For Freelancers
         </h1>
         <div className="flex justify-between gap-5">
           <Courses handleCourse={handleCourse}></Courses>
